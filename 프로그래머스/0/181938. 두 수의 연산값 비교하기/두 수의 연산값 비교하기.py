4747cdb12acc8ee25afdc73061fbbc042c8a3ee6
amnyday <psystudy5783@gmail.com>
2024-08-16 10:56:15 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,7 +1,5 @@
 def solution(a, b):
-    if int(f'{a}{b}') > 2 * a* b :
-        answer = int(f'{a}{b}')
-    elif int(f'{a}{b}') == 2 * a* b :
+    if int(f'{a}{b}') >= 2 * a* b :
         answer = int(f'{a}{b}')
     else:
         answer = 2 * a * b
