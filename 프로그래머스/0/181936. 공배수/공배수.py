db0679e342079fd2d8c1128d8b59adfead33ccf7
amnyday <psystudy5783@gmail.com>
2024-08-16 17:43:03 +0900

    [level 0] Title: 공배수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -0,0 +1,8 @@
+def solution(number, n, m):
+    return 1 if number%n ==0 and number %m ==0 else 0
+
+    # if number%n == 0 and number %m ==0:
+    #     answer = 1
+    # else:
+    #     answer = 0
+    # return answer
