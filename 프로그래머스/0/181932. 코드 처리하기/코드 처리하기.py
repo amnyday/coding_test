c9eb00563c1a96d48192183bd1a434756b89ded2
amnyday <psystudy5783@gmail.com>
2024-09-01 21:20:17 +0900

    [level 0] Title: 코드 처리하기, Time: 20.42 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,18 @@
+def solution(code):
+    mode = 0
+    ret =''
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i] == "1":
+                mode = 1
+            else:
+                if i%2 ==0 :
+                    ret += code[i]
+        elif mode ==1:
+            if code[i] == "1":
+                mode = 0
+            else:
+                if i%2 != 0:
+                    ret += code[i]
+                
+    return 'EMPTY' if len(ret) ==0 else ret
