d16de62dd96919b05c28361c7f3210cff3165db2
amnyday <psystudy5783@gmail.com>
2024-11-11 23:09:39 +0900

    [level 0] Title: 수 조작하기 2, Time: 18.82 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,13 +1,18 @@
 def solution(numLog):
     answer = ''
+    flag = dict(zip([1,-1,10,-10], ['w','s','d','a']))
     for i in range(1, len(numLog)):
-        diff = numLog[i] - numLog[i-1]
-        if diff == 1:
-            answer += 'w'
-        elif diff == -1:
-            answer += 's'
-        elif diff == 10:
-            answer += 'd'
-        elif diff == -10:
-            answer += 'a'
+        answer += flag[numLog[i] - numLog[i-1]]
     return answer
+    # answer = ''
+    # for i in range(1, len(numLog)):
+    #     diff = numLog[i] - numLog[i-1]
+    #     if diff == 1:
+    #         answer += 'w'
+    #     elif diff == -1:
+    #         answer += 's'
+    #     elif diff == 10:
+    #         answer += 'd'
+    #     elif diff == -10:
+    #         answer += 'a'
+    #return answer
